c7114852a928ab4f44ce1e5406bf019afcc09105
Your Name <you@example.com>
2025-03-16 10:21:44 +0000

    login file

---
+++ flipkart/src/main/resources/archetype-resources/src/main/java/login.java
@@ -0,0 +1,14 @@
+package $package;
+
+/**
+ * Hello world!
+ *
+ */
+public class login
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
